e7e3bd3acf9f687ec61ab3896230ab15e14c999b
Tommy <44683197+Tommysvarva@users.noreply.github.com>
2020-01-26 00:07:02 +0100

    26.01.2020

---
+++ FlaskeSimulator/FlaskeSimulator/Bottle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace FlaskeSimulator
+{
+    public class Bottle
+    {
+        public int capacity;
+        public int content;
+        public Bottle(int capacityValue, int contentValue)
+        {
+            this.capacity = capacityValue;
+            this.content = contentValue;
+        }
+
+        public void FillFlask()
+        {
+            content = capacity;
+            Console.WriteLine(content);
+
+        }
+    }
+}
---
+++ FlaskeSimulator/FlaskeSimulator/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace FlaskeSimulator
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var bottle1 = new Bottle(10, 5);
+            var bottle2 = new Bottle(20, 3);
+            var totalVolume = 5;
+            Console.WriteLine($"bottle1 capacity = {bottle1.capacity}");
+            Console.WriteLine($"bottle1 content = {bottle1.content}");
+            Console.WriteLine($"bottle2 capacity = {bottle2.capacity}");
+            Console.WriteLine($"bottle2 content = {bottle2.content}");
+            bottle1.FillFlask();
+        }
+    }
+}
